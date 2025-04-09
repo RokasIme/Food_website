4f5912c092525e547d9f9de8afb2729f5c617aa3
Rokas <Rokas.Imelinskas@gmail.com>
2025-04-09 08:28:45 +0300

    pizza

--- js/pizza.js
+++ js/pizza.js
@@ -1,36 +1,36 @@
 export const foodList = [
   {
-    photo: "../img/pizza.webp",
+    photo: "./img/pizza.webp",
     name: "Margarita",
     price: 449,
     availability: 4,
   },
   {
-    photo: "../img/pizza.webp",
+    photo: "./img/pizza.webp",
     name: "4 seasons",
     price: 1299,
     availability: 1,
   },
   {
-    photo: "../img/pizza.webp",
+    photo: "./img/pizza.webp",
     name: "Mafia",
     price: 1699,
     availability: 0,
   },
   {
-    photo: "../img/pizza.webp",
+    photo: "./img/pizza.webp",
     name: "4 cheese",
     price: 1313,
     availability: 13,
   },
   {
-    photo: "../img/pizza.webp",
+    photo: "./img/pizza.webp",
     name: "Ham & Mushrooms",
     price: 449,
     availability: 5,
   },
   {
-    photo: "../img/pizza.webp",
+    photo: "./img/pizza.webp",
     name: "Havaju",
     price: 899,
     availability: 18,
